e1bc915907e9b3da3b7a3acac83fe5ad3aff8310
Mori <morionesrein@gmail.com>
2026-02-27 16:15:32 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -36,3 +36,32 @@ public class Person {
     }
 
 }
+
+// Person.java (With Constructor)
+
+public class Person {
+    private String name;
+    private int age;
+
+    //Getters Method
+    public String getName() {
+        return name;
+    }
+
+    //Setters Method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Invalid age!");
+        }
+    }
+}
