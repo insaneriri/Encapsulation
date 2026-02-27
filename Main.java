4892fe80645926154622e48ba4313ffac8e31015
Mori <morionesrein@gmail.com>
2026-02-27 15:47:12 +0800

    Main.java

---
+++ Main.java
@@ -0,0 +1,38 @@
+// Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        Person iPerson = new Person();
+        iPerson.setName("Yuuta");
+        iPerson.setAge(19);
+
+        System.out.println("Name: " + iPerson.getName());
+        System.out.println("Age: " + iPerson.getAge());
+    }
+}
+
+// Person.java
+
+public class Person {
+    private String name;
+    private int age;
+
+    //Getters Method
+    public String getName() {
+        return name;
+    }
+
+    //Setters Method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
